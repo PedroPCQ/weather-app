1291ec787fcca18c26d5884be43d10c397eb37e4
Pedro Calvo Quesada <pedrocalvoquesada@gmail.com>
2020-09-13 20:25:21 +0200

    Funciones JS

---
+++ js/main.js
@@ -0,0 +1,52 @@
+'use stict';
+
+const api = {
+  key: "4ce8209c576871b74e11b2fa627737e2",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchBox = document.querySelector('.search-box');
+searchBox.addEventListener('keypress', setQuery);
+
+function setQuery(event) {
+  if (event.keyCode == 13) {
+    getResults(searchBox.value);
+  }
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+  .then(weather => {
+    return weather.json();
+  }).then(displayResults);
+}
+
+function displayResults(weather) {
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date =document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>ºC</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].main;
+
+  let highLow = document.querySelector('.high-low');
+  highLow.innerText = `${Math.round(weather.main.temp_min)}ºC / ${Math.round(weather.main.temp_max)}ºC`
+}
+
+function dateBuilder(d) {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "sunday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
